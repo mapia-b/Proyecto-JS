15e2466cb07d9c2c13756eba84a82bb885a43435
mapia-b <mapia.barreiro@hotmail.com>
2023-08-26 16:53:51 -0300

    uso de API

--- js/array.js
+++ js/array.js
@@ -37,8 +37,41 @@ function verPacientes () {
 boton.addEventListener("click", () => {
     nuevoPaciente()
     verPacientes()
+    Swal.fire({
+        icon: 'success', 
+        title: 'Correcto',
+        text: 'Nuevo paciente agregado al listado',     
+      })
+     
 })
 
+//Trabajo con API: fecha y hora
+document.addEventListener ("DOMContentLoaded", function (){
+    const fecha = document.getElementById ("date")
+    const url ="http://worldtimeapi.org/api/timezone/America/Argentina/Buenos_Aires"
+    fetch (url)
+    .then (response => response.json())
+    .then (data => {
+        //cambio formato de como se muestran los datos
+        const fechaHora = new Date(data.datetime);
+        const formatoFechaHora = {
+            day: "numeric",
+            month: "numeric",
+            year: "numeric",
+            hour: "numeric",
+            minute: "numeric",
+            second: "numeric",
+           
+        }
+        const datosCambiados = fechaHora.toLocaleDateString ("es-UY", formatoFechaHora)
+
+        fecha.textContent = `Datos cargados en la fecha y hora: ${datosCambiados}`;
+    })
+    .catch ( error => {
+        console.error("Error:", error);
+      fecha.textContent = "Error, no se pudo obtener la informacion requerida";
+    })
+})
 
 
 
